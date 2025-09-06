e24e7b556e0a5090e54b70958ee43466bbac9bbe
bibek190 <bibek9818917036@gmail.com>
2025-09-06 15:23:23 +1000

    edit top seller to learn github merge

--- frontend/src/pages/home/TopSeller.jsx
+++ frontend/src/pages/home/TopSeller.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const TopSeller = () => {
-  return <div>top seller</div>;
+  return <div>Hello i want to push this from staging to main branch</div>;
 };
 
 export default TopSeller;
