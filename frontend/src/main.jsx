a5be59906363ea3d78a0acde35c10b21e42d6420
bibek190 <bibek9818917036@gmail.com>
2025-09-06 14:35:47 +1000

    feat(banner):add new banner section and change tailwind styles

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,6 +4,7 @@ import "./index.css";
 import App from "./App.jsx";
 import { RouterProvider } from "react-router-dom";
 import router from "./routers/router.jsx";
+import "./App.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,16 @@
+export default {
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx,css}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#ffce1a",
+        secondary: "#0d0842",
+      },
+      fontFamily: {
+        primary: ["Montserrat", "sans-serif"],
+        secondary: ["Nunito Sans", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
