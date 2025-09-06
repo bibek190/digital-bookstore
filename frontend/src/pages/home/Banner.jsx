12eb7929349c38a3d4fdd3e30757f9b2f4fdaaa8
bibek190 <bibek9818917036@gmail.com>
2025-09-07 02:30:20 +1000

    feat:(banner) add bookCard,add books api

--- frontend/src/pages/home/Banner.jsx
+++ frontend/src/pages/home/Banner.jsx
@@ -4,7 +4,7 @@ import bannerImg from "../../assets/banner.png";
 const Banner = () => {
   return (
     <div className="flex flex-col md:flex-row-reverse py-16 px-3 justify-between items-center gap-4">
-      <div className=" w-full flex items-center md:justify-end">
+      <div className=" w-1/2 flex items-center md:justify-end">
         <img src={bannerImg} alt="banner" />
       </div>
       <div>
--- frontend/src/pages/home/TopSeller.jsx
+++ frontend/src/pages/home/TopSeller.jsx
@@ -1,7 +1,45 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import BookCard from "../books/BookCard";
+
+const categories = ["Choose a genre", "Business", "Fiction", "adventure"];
 
 const TopSeller = () => {
-  return <div>Hello i want to push this from staging to main branch</div>;
+  const [books, setBooks] = useState([]);
+  const [selectedCategory, setSelectedCategory] = useState("Choose a genre");
+
+  useEffect(() => {
+    fetch("/book.json")
+      .then((res) => res.json())
+      .then((data) => setBooks(data));
+  }, []);
+
+  const filteredBooks =
+    selectedCategory === "Choose a genre"
+      ? books
+      : books.filter(
+          (book) => book.category === selectedCategory.toLowerCase()
+        );
+
+  return (
+    <div className="py-10">
+      <h2 className=" text-3xl font-semibold mb-6">Top Seller</h2>
+      <div>
+        <select
+          name="category"
+          id="category"
+          className="px-4 py-2 outline-none border bg-[#eaeaea] border-gray-300 rounded-md"
+          onChange={(e) => setSelectedCategory(e.target.value)}
+        >
+          {categories.map((category, index) => {
+            return <option key={index}>{category}</option>;
+          })}
+        </select>
+      </div>
+      {filteredBooks.map((book, index) => {
+        return <BookCard key={index} book={book} />;
+      })}
+    </div>
+  );
 };
 
 export default TopSeller;
